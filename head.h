1f81de1924352285d4a615cae0129cc732518c86
Risky Kurniawan <62739625+riskykurniawan15@users.noreply.github.com>
2020-07-13 10:40:38 +0700

    VERSION 1.1
    upload base version

---
+++ head.h
@@ -0,0 +1,27 @@
+/*
+  Pembuat    : Risky Kurniawan
+  Nim        : 16180001
+  Email      : Riskykurniawan@programmer.net / Riskykurniawan15@gmail.com
+  Kontak     : 083826114233
+  Pembimbing : Salman Topiq S.Kom, M.Kom
+  Diajukan   : Tugas Akhir Dasar Pemrograman
+*/
+#include <conio.h>
+#include <iostream.h>
+
+head(char jud[50]=""){
+	cout<<"***************************************************************"<<endl;
+   cout<<".....................::PEGADAIAN SYARIAH::....................."<<endl;
+	cout<<"                       "<<jud<<endl;
+  	cout<<"***************************************************************"<<endl;
+   if(strcmp(jud,"LOGIN APLIKASI")==0){
+    	cout<<"JUDUL     : APP PEGADAIAN SYARIAH"<<endl;
+   	cout<<"PEMBUAT   : RISKY KURNIAWAN"<<endl;
+   	cout<<"NIM       : 16180001"<<endl;
+   	cout<<"KELAS     : 16.1A.33"<<endl;
+      cout<<"KELOMPOK  : 9"<<endl;
+   	cout<<"VERSI APP : 1.1 (BUILD FULL WITH BORLAND C++)"<<endl;
+      cout<<"NOTE      : Gunakan (Username:risky) dan (Password:kurniawan)"<<endl;
+   	cout<<"***************************************************************"<<endl;
+   }
+}
